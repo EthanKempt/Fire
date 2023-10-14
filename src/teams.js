d5529981a865dd8eb28c9e44f4ba4b060749754a
Ethan Kempton <ethankempton0@gmail.com>
2023-10-13 18:43:50 -0700

    teams js and bundle2

--- src/teams.js
+++ src/teams.js
@@ -17,28 +17,15 @@ initializeApp(firebaseConfig)
 
 const db = getFirestore()
 
-const colRef = collection(db, 'names')
+const colRef = collection(db, 'teams')
 
 getDocs(colRef)
   .then((snapshot) => {
-    window.names = []
+    window.teams = []
     snapshot.docs.forEach((doc) => {
-        names.push({ ...doc.data(), id: doc.id })
+        teams.push({ ...doc.data(), id: doc.id })
     })
   })
   .catch(err => {
     console.log(err.message)
   })
-
-  window.login = function() {
-    userCheck(); //sets userIn and UserInP to current input
-    for (let i = 0; i < names.length; i++) {
-       if (userIn == names[i].name && userInP == names[i].password) {
-        sessionStorage.setItem('currentUser', names[i].name);
-        window.location.href = '/html/home.html';
-        return true; //stops code from continuing
-      } else if (i + 1 == names.length) {
-        alert('incorrect username or password');
-       }
-    }
-  }
--- webpack.config.js
+++ webpack.config.js
@@ -9,3 +9,15 @@ module.exports = {
     },
     watch: true
 }
+
+const path2 = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/teams.js',
+    output: {
+        path: path2.resolve(__dirname, 'dist'),
+        filename: 'bundleTeams.js'
+    },
+    watch: true
+}
